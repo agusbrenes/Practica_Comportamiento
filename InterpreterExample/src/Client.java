e5fbd0d6de37fa8b168d8f4a74e863642a53300f
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-05-30 20:06:39 -0600

    Update Client.java

--- InterpreterExample/src/Client.java
+++ InterpreterExample/src/Client.java
@@ -1,6 +1,14 @@
 public class Client {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String operation = scan.nextLine();
+        String[] tokens = operation.split("[ ]");
+        for (String tok: tokens) {
+            System.out.println(tok);
+        }
+        Interpreter interpreter = new Interpreter(tokens);
+        interpreter.interpret();
         
     }
 }
