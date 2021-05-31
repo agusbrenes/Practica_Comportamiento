1cecc554eb9aadc7ded3df89f330d541304e87c2
agusbrenes1 <>
2021-05-31 01:51:26 +0000

    Falta main

--- InterpreterExample/src/User.java
+++ InterpreterExample/src/User.java
@@ -1,6 +1,19 @@
-public class User {
+public class Interpreter {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void Interpreter(String[] args) {
+        Context context = new Context();
+        Expression exp;
+        
+		for (String token: operacion) {
+            if (context.getInteger(token) >= 0) {
+                exp = new NumericExpression(token);
+            } else {
+                exp = new OperationExpression(token);
+            }
+
+            exp.interpret(context);
+        }
+
+        System.out.println("The operation's result is: " + context.getResult());
     }
 }
